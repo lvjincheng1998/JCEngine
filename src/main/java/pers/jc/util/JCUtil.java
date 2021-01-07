083de6433b116652a909a704cb6f42103d161d06
lvjincheng <48816275+lvjincheng1998@users.noreply.github.com>
2021-01-07 23:59:01 +0800

    add cache and timeout function

--- src/main/java/pers/jc/util/JCUtil.java
+++ src/main/java/pers/jc/util/JCUtil.java
@@ -24,15 +24,34 @@ public class JCUtil {
 		return simpleDateFormat.format(new Date(timestamp13));
 	}
 
-	public static String getDatetimeStr(int timestamp10) {
-		return getDatetimeStr((long)timestamp10 * 1000,"yyyy-MM-dd hh:mm:ss");
+	public static String getDatetimeStr() {
+		return getDatetimeStr("yyyy-MM-dd HH:mm:ss");
 	}
 
-	public static String getDatetimeStr() {
-		return getDatetimeStr("yyyy-MM-dd hh:mm:ss");
+	public static String getDatetimeStr(int timestamp10) {
+		return getDatetimeStr((long)timestamp10 * 1000,"yyyy-MM-dd HH:mm:ss");
+	}
+
+	public static String getDatetimeStr(long timestamp13) {
+		return getDatetimeStr((long)timestamp13,"yyyy-MM-dd HH:mm:ss");
 	}
 
 	public static String getDateStr() {
 		return getDatetimeStr("yyyy-MM-dd");
+	}
+
+	public static String getDateStr(int timestamp10) {
+		return getDatetimeStr((long) timestamp10 * 1000, "yyyy-MM-dd");
+	}
+
+	public static String getDateStr(long timestamp13) {
+		return getDatetimeStr(timestamp13, "yyyy-MM-dd");
+	}
+
+	public static int[] getTimeHMS(int second) {
+		int hours = second / 3600;
+		int minutes = second % 3600 / 60;
+		int seconds = second % 60;
+		return new int[] {hours, minutes, seconds};
 	}
 }
