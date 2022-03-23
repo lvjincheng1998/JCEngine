43dea6c8d62da73081f7073bd451cb1722b4b250
lvjincheng <48816275+lvjincheng1998@users.noreply.github.com>
2022-03-23 19:17:07 +0800

    Update JCEngine.cs

--- JCEngine.cs
+++ JCEngine.cs
@@ -2,77 +2,62 @@ using System;
 using System.Collections.Generic;
 using BestHTTP.WebSocket;
 using Newtonsoft.Json;
-
-public class JCEngine {
-    public static string url;
-    public static Type entityClass;
-
-    public static void boot(string url, Type entityClass) {
-        JCEngine.url = url;
-        JCEngine.entityClass = entityClass;
-        new JCEngineCore.WebSocketServer(url, null);
-    }
-
-    public static void reBoot(JCEntity entity) {
-        new JCEngineCore.WebSocketServer(url, entity);
-    }
-}
-
-public class JCEntity {
-    public int id;
-    public JCEngineCore.Channel channel;
-    public bool isValid;
-    public bool loaded;
-    public Dictionary<string, object> components = new Dictionary<string, object>();
-
-    public virtual void onLoad() {}
-
-    public virtual void onReload() {}
-
-    public virtual void onDestroy() {}
-
-    public virtual void onMiss() {}
-
-    public bool call(string func, params object[] args) {
-        return call(func, args, null);
-    }
-
-    public bool call(string func, object[] args = null, Delegate callback = null) {
-        if (this.isValid) {
-            string uuid = "";
-            int type = Convert.ToInt32(JCEngineCore.DataType.FUNCTION);
-            if (func.IndexOf(".") > -1) {
-                type = Convert.ToInt32(JCEngineCore.DataType.METHOD);
-                uuid = JCEngineCore.CallbackHandler.addCallback(callback);
-            }
-            if (args == null) {
-                args = new object[]{};
-            }
-            JCEngineCore.Data data = new JCEngineCore.Data();
-            data.uuid = uuid;
-            data.type = type;
-            data.func = func;
-            data.args = args;
-            this.channel.writeAndFlush(JsonConvert.SerializeObject(data));
-            return true;
-        }
-        return false;
-    }
-}
+using Newtonsoft.Json.Linq;
 
 namespace JCEngineCore {
-    
+    public class JCEngine {
+        private static string url;
+        public static void boot(string url, JCEntity entity) {
+            JCEngine.url = url;
+            new JCEngineCore.WebSocketServer(url, entity);
+        }
+        public static void reboot(JCEntity entity) {
+            new JCEngineCore.WebSocketServer(url, entity);
+        }
+    }
+    public class JCEntity {
+        public int id;
+        public JCEngineCore.Channel channel;
+        public bool isValid;
+        public bool loaded;
+        public Dictionary<string, object> components = new Dictionary<string, object>();
+        public virtual void onLoad() {}
+        public virtual void onReload() {}
+        public virtual void onDestroy() {}
+        public virtual void onMiss() {}
+        public bool call(string func, params object[] args) {
+            return call(func, args, null);
+        }
+        public bool call(string func, object[] args = null, Delegate callback = null) {
+            if (this.isValid) {
+                string uuid = "";
+                int type = Convert.ToInt32(JCEngineCore.DataType.FUNCTION);
+                if (func.IndexOf(".") > -1) {
+                    type = Convert.ToInt32(JCEngineCore.DataType.METHOD);
+                    uuid = JCEngineCore.CallbackHandler.addCallback(callback);
+                }
+                if (args == null) {
+                    args = new object[]{};
+                }
+                JCEngineCore.Data data = new JCEngineCore.Data();
+                data.uuid = uuid;
+                data.type = type;
+                data.func = func;
+                data.args = args;
+                this.channel.writeAndFlush(JsonConvert.SerializeObject(data));
+                return true;
+            }
+            return false;
+        }
+    }
     public class Channel {
         private WebSocket webSocket;
-
         public Channel (WebSocket webSocket) {
             this.webSocket = webSocket;
         }
-
         public void writeAndFlush(string text) {
             this.webSocket.Send(text);
         }
-
         public void close() {
             this.webSocket.Close();
         }
@@ -80,31 +65,24 @@ namespace JCEngineCore {
     class WebSocketServer {
         private WebSocket webSocket;
         private JCEntity tempEntity;
-    
         public WebSocketServer(string url, JCEntity entity) {
             this.webSocket = new WebSocket(new Uri(url));
             this.webSocket.StartPingThread = true;
-            this.tempEntity = entity != null ? entity : JCEngine.entityClass.Assembly.CreateInstance(JCEngine.entityClass.FullName) as JCEntity;
-
+            this.tempEntity = entity;
             this.webSocket.OnOpen += delegate(WebSocket webSocket) {
                 this.call("loadTempEntity");
             };
-    
-            this.webSocket.OnClosed = delegate(WebSocket webSocket, UInt16 code, string message) {
+            this.webSocket.OnClosed += delegate(WebSocket webSocket, UInt16 code, string message) {
                 this.destroyTempEntity();
             };
-
-            this.webSocket.OnError = delegate(WebSocket webSocket, Exception ex) {
+            this.webSocket.OnError += delegate(WebSocket webSocket, Exception ex) {
                 this.destroyTempEntity();
             };
-    
-            this.webSocket.OnMessage = delegate(WebSocket webSocket, string message) {
+            this.webSocket.OnMessage += delegate(WebSocket webSocket, string message) {
                 this.invoke(JsonConvert.DeserializeObject<Data>(message));            
             };
-
             this.webSocket.Open();
         }
-    
         private void call(string func, object[] args = null) {
             if (args == null) {
                 args = new object[]{};
@@ -116,12 +94,11 @@ namespace JCEngineCore {
             data.args = args;
             this.webSocket.Send(JsonConvert.SerializeObject(data));
         }
-    
         private void invoke(Data data) {
             DataType dataType = (DataType) data.type;
             if (dataType == DataType.EVENT) {
                 System.Reflection.MethodInfo method = this.GetType().GetMethod(data.func);
-                Utility.formatArgsType(data.args, method.GetParameters());
+                Utility.FormatArgsType(data.args, method.GetParameters());
                 method.Invoke(this, data.args);
                 return;
             }
@@ -136,14 +113,13 @@ namespace JCEngineCore {
                         object matchContext;
                         this.tempEntity.components.TryGetValue(key, out matchContext);
                         if (matchContext != null) {
-                            string[] arr = func.Split(new char[]{'.'});
-                            func = arr[arr.Length - 1];
+                            func = func.Substring(pointIndex + 1);
                             context = matchContext;
                         }
                     }
                     if (context != null) {
                         System.Reflection.MethodInfo method = context.GetType().GetMethod(func);
-                        Utility.formatArgsType(data.args, method.GetParameters());
+                        Utility.FormatArgsType(data.args, method.GetParameters());
                         method.Invoke(context, data.args);
                     }
                 }
@@ -153,7 +129,6 @@ namespace JCEngineCore {
                 CallbackHandler.handleCallback(data);
             }
         }
-    
         public void loadTempEntity(int id) {
             this.tempEntity.id = id;
             this.tempEntity.channel = new Channel(this.webSocket);
@@ -167,7 +142,6 @@ namespace JCEngineCore {
             } catch (Exception) {}
             this.tempEntity.loaded = true;
         }
-    
         public void destroyTempEntity() {
             if (this.tempEntity.isValid) {
                 this.tempEntity.isValid = false;
@@ -177,16 +151,13 @@ namespace JCEngineCore {
             }
         }
     }
-
     class CallbackHandler {
         private static int nextID = 0;
         private static Dictionary<string, CallbackInfo> mapper = new Dictionary<string, CallbackInfo>();
-    
         private static string uuid() {
             nextID++;
             return nextID.ToString();
         }
-    
         public static string addCallback(Delegate callback) {
             string uuid = CallbackHandler.uuid();
             if (callback != null) {
@@ -197,7 +168,6 @@ namespace JCEngineCore {
             }
             return uuid;
         }
-    
         public static void handleCallback(Data data) {
             if (mapper.Count > 10) {
                 long now = Utility.GetTimestamp();
@@ -217,38 +187,48 @@ namespace JCEngineCore {
                 mapper.Remove(data.uuid);
                 object target = callbackInfo.callback.Target;
                 System.Reflection.MethodInfo method = callbackInfo.callback.Method;
-                Utility.formatArgsType(data.args, method.GetParameters());
+                Utility.FormatArgsType(data.args, method.GetParameters());
                 method.Invoke(target, data.args);
             }
         }
     }
-
     class Utility {
         public static long GetTimestamp() {
             TimeSpan ts = DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1);
             return (long)ts.TotalMilliseconds;
         }
-        public static void formatArgsType(object[] args, System.Reflection.ParameterInfo[] parameters) {
+        public static void FormatArgsType(object[] args, System.Reflection.ParameterInfo[] parameters) {
             int i = 0;
             foreach (var param in parameters) {
-                args[i] = Convert.ChangeType(args[i], param.ParameterType);
+                Type type = param.ParameterType;
+                if (IsBaseType(type)) {
+                    args[i] = Convert.ChangeType(args[i], type);
+                } else if (args[i].GetType().IsSubclassOf(typeof(JContainer))) {
+                    args[i] = ((JContainer)args[i]).ToObject(type);
+                }
                 i++;
             }
         }
+        public static bool IsBaseType(Type type) {
+            if (typeof(System.Int32).Equals(type)) return true;
+            if (typeof(System.Int64).Equals(type)) return true;
+            if (typeof(System.Single).Equals(type)) return true;
+            if (typeof(System.Double).Equals(type)) return true;
+            if (typeof(System.String).Equals(type)) return true;
+            if (typeof(System.Boolean).Equals(type)) return true;
+            return false;
+        }
     }
-
     class CallbackInfo {
         public Delegate callback;
         public long deadTime;
     }
-    
     class Data {
         public string uuid;
         public int type;
         public string func;
         public object[] args;
     }
-    
     enum DataType {
         EVENT,
         FUNCTION,
