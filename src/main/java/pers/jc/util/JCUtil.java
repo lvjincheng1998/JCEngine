fd34b88ede61a45fbc423e463c02cfd2d6ddcef5
lvjincheng <48816275+lvjincheng1998@users.noreply.github.com>
2022-12-03 01:07:17 +0800

    Update JCUtil.java

--- src/main/java/pers/jc/util/JCUtil.java
+++ src/main/java/pers/jc/util/JCUtil.java
@@ -7,4 +7,8 @@ public class JCUtil {
 	public static String uuid() {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
+
+	public static boolean isLinux() {
+		return System.getProperties().getProperty("os.name").startsWith("Linux");
+	}
 }
