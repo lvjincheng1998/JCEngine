5b27059be0524c4ae27f0d3af9535e7feaa68555
lvjincheng <48816275+lvjincheng1998@users.noreply.github.com>
2023-01-28 18:22:11 +0800

    Update JCCacheMap.java

--- src/main/java/pers/jc/util/JCCacheMap.java
+++ src/main/java/pers/jc/util/JCCacheMap.java
@@ -1,10 +1,15 @@
 package pers.jc.util;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.io.File;
 import java.util.*;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class JCCacheMap {
-    private final Map<String, CacheMapValue> cacheMap = new HashMap<>();
+    private Map<String, CacheMapValue> cacheMap = new HashMap<>();
     private final ReentrantReadWriteLock cacheMapLock = new ReentrantReadWriteLock();
 
     public JCCacheMap() {
@@ -93,7 +98,7 @@ public class JCCacheMap {
         return cacheMapValue != null && (cacheMapValue.timeout == -1 || System.currentTimeMillis() < cacheMapValue.timeout);
     }
 
-    private static List<JCCacheMap> autoClearMapList = new LinkedList<>();
+    private static final List<JCCacheMap> autoClearMapList = new LinkedList<>();
     private static boolean autoClearStarted = false;
     private static final long autoClearInterval = 10 * 1000;
 
@@ -123,13 +128,51 @@ public class JCCacheMap {
         }
     }
 
-    private static class CacheMapValue {
-        Object value;
-        long timeout;
+    public static class CacheMapValue {
+        @JSONField
+        public Object value;
+        @JSONField
+        public long timeout;
 
-        CacheMapValue(Object value, long timeout) {
+        public CacheMapValue() {}
+
+        public CacheMapValue(Object value, long timeout) {
             this.value = value;
             this.timeout = timeout;
+        }
+    }
+
+    public void saveTo(String dirPath, String fileNameNoSuffix) {
+        File dir = new File(dirPath);
+        if (!dir.exists()) dir.mkdirs();
+        try {
+            cacheMapLock.readLock().lock();
+            File file = new File(dirPath + File.separator + fileNameNoSuffix + ".json");
+            if (!file.exists()) file.createNewFile();
+            JCFileTool.writeStr(file, JSON.toJSONString(cacheMap));
+        } catch (Exception e) { e.printStackTrace(); }
+        finally {
+            cacheMapLock.readLock().unlock();
+        }
+    }
+
+    public void loadFrom(String dirPath, String fileNameNoSuffix) {
+        File file = new File(dirPath + File.separator + fileNameNoSuffix + ".json");
+        if (!file.exists()) return;
+        try {
+            cacheMapLock.writeLock().lock();
+            JSONObject jsonMap = JSON.parseObject(JCFileTool.readStr(file));
+            for (Map.Entry<String, Object> entry : jsonMap.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                if (value instanceof JSONObject) {
+                    CacheMapValue cacheMapValue = ((JSONObject) value).toJavaObject(CacheMapValue.class);
+                    cacheMap.put(key, cacheMapValue);
+                }
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        finally {
+            cacheMapLock.writeLock().unlock();
         }
     }
 }
